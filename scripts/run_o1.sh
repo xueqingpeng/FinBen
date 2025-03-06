230e392cae3b618831e8151a110470d4ec445fb9
xueqing <xueqing.peng@yale.edu>
2025-03-06 11:57:05 -0600

    QwQ-32B

--- scripts/run_o1.sh
+++ scripts/run_o1.sh
@@ -2,19 +2,20 @@
 
 source .env
 export VLLM_WORKER_MULTIPROC_METHOD="spawn"
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=2,3
 
 # Array of models
 MODELS=(
-    "deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
-    "meta-llama/Llama-3-70B-Instruct"
-    "meta-llama/Llama-3.1-70B-Instruct"
-    "meta-llama/Llama-3.3-70B-Instruct"
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
-    "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
-    "meta-llama/Meta-Llama-3-8B-Instruct"
-    "Qwen/Qwen2.5-72B-Instruct-math"
+    # "deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
+    # "meta-llama/Llama-3-70B-Instruct"
+    # "meta-llama/Llama-3.1-70B-Instruct"
+    # "meta-llama/Llama-3.3-70B-Instruct"
+    # "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"
+    # "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+    # "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
+    # "meta-llama/Meta-Llama-3-8B-Instruct"
+    # "Qwen/Qwen2.5-72B-Instruct-math"
+    "Qwen/QwQ-32B"
 )
 
 # Loop through each model
@@ -22,8 +23,8 @@ for MODEL in "${MODELS[@]}"; do
     echo "Evaluating model: $MODEL"
 
     lm_eval --model vllm \
-        --model_args "pretrained=$MODEL,tensor_parallel_size=4,gpu_memory_utilization=0.8" \
-        --tasks complong \
+        --model_args "pretrained=$MODEL,tensor_parallel_size=2,gpu_memory_utilization=0.8" \
+        --tasks fin \
         --batch_size auto \
         --output_path ../results/o1 \
         --hf_hub_log_args "hub_results_org=TheFinAI,details_repo_name=lm-eval-results-o1,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False" \

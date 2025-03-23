ff9d41869126398aea6114fba7439aea8cd12d36
xueqing_misha <xueqing.peng@yale.edu>
2025-03-23 00:45:45 -0400

    misha_test

---
+++ scripts/call_misha.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=grfinsum
+#SBATCH --time=02-00:00:00
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --gpus=4
+#SBATCH --constraint="h100"
+#SBATCH --mem=512G
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/xp83/Documents/project/logs/%j_gpu.out
+
+module load miniconda
+mamba activate finben
+
+echo '-------------------------------------------------'
+echo "Job Name: ${SLURM_JOB_NAME}"
+echo "I have ${SLURM_CPUS_ON_NODE} CPUs on node $(hostname -s) on partition ${SLURM_JOB_PARTITION}"
+echo ${SLURM_SUBMIT_DIR}
+echo Running on host $(hostname)
+echo Time is $(date)
+echo SLURM_NODES are $(echo ${SLURM_NODELIST})
+echo '-------------------------------------------------'
+echo -e '\n\n'
+
+bash "/gpfs/radev/home/xp83/Documents/project/scripts/FinBen/scripts/run_gr.sh"

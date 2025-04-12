7bc4b2ed524ae5c3602ca01fc626daea74e8f30b
xueqing <xueqing.peng@yale.edu>
2025-04-11 23:47:01 -0500

    fl

--- scripts/run_fl.sh
+++ scripts/run_fl.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 source .env
-echo "HF_USERNAME: $HF_USERNAME"
 echo "HF_TOKEN: $HF_TOKEN" | cut -c1-20
 
 export VLLM_WORKER_MULTIPROC_METHOD="spawn"
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1
+# export VLLM_IGNORE_FUSION_LAYER=1
+export PYTHONWARNINGS="ignore"
 
 # Array of models
 MODELS=(
-    # "Qwen/Qwen2.5-3B"
-    # "Qwen/Qwen2.5-3B-Instruct"
-    "Xueqing/fl-cleveland-dpo-qlora"
-    # "Xueqing/fl-hungarian-dpo-qlora"
-    # "Xueqing/fl-switzerland-dpo-qlora"
-    # "Xueqing/fl-va-dpo-qlora"
+    "Qwen/Qwen2.5-3B"
+    "Qwen/Qwen2.5-3B-Instruct"
+    "TheFinAI/fl-cleveland-sft-2"
+    "TheFinAI/fl-switzerland-sft-2"
+    "TheFinAI/fl-hungarian-sft-2"
+    "TheFinAI/fl-va-sft-2"
 )
 
 # Loop through each model
@@ -23,10 +24,10 @@ for MODEL in "${MODELS[@]}"; do
 
     # 1024
     lm_eval --model vllm \
-        --model_args "pretrained=$MODEL,tensor_parallel_size=1,gpu_memory_utilization=0.8,max_model_len=1024" \
+        --model_args "pretrained=$MODEL,tensor_parallel_size=2,gpu_memory_utilization=0.8,max_model_len=1024" \
         --tasks fl \
         --batch_size auto \
-        --output_path ../results \
+        --output_path ../results/fl \
         --hf_hub_log_args "hub_results_org=TheFinAI,details_repo_name=lm-eval-results-fl-0shot,push_results_to_hub=False,push_samples_to_hub=False,public_repo=False" \
         --log_samples \
         --apply_chat_template \

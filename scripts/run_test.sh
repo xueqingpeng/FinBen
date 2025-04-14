1048a077da802481bb2f4ff6f713035eac9d281b
xueqing <xueqing.peng@yale.edu>
2025-04-13 22:34:17 -0500

    dolfin

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -4,15 +4,16 @@ source .env
 echo "HF_TOKEN: $HF_TOKEN"
 
 export VLLM_WORKER_MULTIPROC_METHOD="spawn"
+export CUDA_VISIBLE_DEVICES=3
 
 MODEL="meta-llama/Llama-3.2-1B-Instruct"
 
 lm_eval --model vllm \
 	--model_args "pretrained=$MODEL,tensor_parallel_size=1,gpu_memory_utilization=0.8,max_model_len=1024" \
-        --tasks gr \
+        --tasks mfb \
         --batch_size auto \
         --output_path ../results \
         --hf_hub_log_args "hub_results_org=TheFinAI,details_repo_name=lm-eval-results,push_results_to_hub=False,push_samples_to_hub=False,public_repo=False" \
         --log_samples \
         --apply_chat_template \
-        --include_path ../tasks/plutus
+        --include_path ../tasks/multifinben
---
+++ tasks/multifinben/multifinben_utils.py
@@ -0,0 +1,25 @@
+from comet import download_model, load_from_checkpoint
+
+def get_comet_score(doc, results):
+    """
+    Calculate the quality score of machine translation using the COMET model.
+    """
+    # Download the model (skip if it already exists)
+    model_path = download_model("wmt20-comet-da")
+    model = load_from_checkpoint(model_path)
+
+    source = doc["text"]
+    mt = results[0].strip()
+    ref = doc["answer"]
+
+    data = [
+        {
+            "src": source, # original text
+            "mt": mt,  # machine translation
+            "ref": ref # human reference translation
+        },
+    ]
+
+    # Predict scores (set gpus=1 to use GPU; gpus=0 for CPU)
+    model_output = model.predict(data, batch_size=8, gpus=1)
+    return {"COMET": model_output.scores[0]}

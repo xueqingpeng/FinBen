1523d69927f387f214b7a4366f2ee05dc8e889f3
xueqing@xp123 <xueqing.peng@yale.edu>
2026-03-17 01:58:20 -0400

    jp_model_list

--- scripts/run_jp.sh
+++ scripts/run_jp.sh
@@ -18,9 +18,16 @@ MODELS=(
     # "Duxiaoman-DI/Llama3.1-XuanYuan-FinX1-Preview"
     # "cyberagent/DeepSeek-R1-Distill-Qwen-32B-Japanese"
 
+    # JP
     # "gpt-5"
-    # "gpt-5-mini"
-    "deepseek-ai/DeepSeek-V3.1"
+    # "gpt-4o"
+    # "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8"
+    # "meta-llama/Llama-3.3-70B-Instruct-Turbo"
+    # "Qwen3 235B-A22B Instruct 2507"
+    # "mistralai/Mistral-Small-24B-Instruct-2501"
+    # "deepseek-ai/DeepSeek-V3.1"
+    # "google/gemma-3n-E4B-it"
+    # "moonshotai/Kimi-K2.5"
 
 )
 
@@ -28,15 +35,16 @@ MODELS=(
 for MODEL in "${MODELS[@]}"; do
     echo "Evaluating model: $MODEL"
     
-    # lm_eval --model vllm \
-    #     --model_args "pretrained=$MODEL,tensor_parallel_size=4,gpu_memory_utilization=0.95,max_model_len=1024" \
-    #     --tasks jp \
-    #     --batch_size auto \
-    #     --output_path ../results/jp \
-    #     --hf_hub_log_args "hub_results_org=TheFinAI,details_repo_name=lm-eval-results-jp,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False" \
-    #     --log_samples \
-    #     --apply_chat_template \
-    #     --include_path ../tasks/jp
+    lm_eval --model vllm \
+        --model_args "pretrained=$MODEL,tensor_parallel_size=4,gpu_memory_utilization=0.95,max_model_len=1024" \
+        --tasks jp_new \
+        --num_fewshot 0 \
+        --batch_size auto \
+        --output_path ../results/jp \
+        --hf_hub_log_args "hub_results_org=TheFinAI,details_repo_name=lm-eval-results-jp,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False" \
+        --log_samples \
+        --apply_chat_template \
+        --include_path ../tasks/jp
         
     # # api-openai
     # lm_eval --model openai-chat-completions \
@@ -50,17 +58,17 @@ for MODEL in "${MODELS[@]}"; do
     #     --apply_chat_template \
     #     --include_path ../tasks/jp
 
-    # api-togetherai
-    lm_eval --model togetherai-chat-completions \
-        --model_args "pretrained=$MODEL, max_tokens=1024" \
-        --tasks jp_new \
-        --num_fewshot 0 \
-        --batch_size auto \
-        --output_path ../results/jp \
-        --hf_hub_log_args "hub_results_org=TheFinAI,details_repo_name=lm-eval-results-jp,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False" \
-        --log_samples \
-        --apply_chat_template \
-        --include_path ../tasks/jp
+    # # api-togetherai
+    # lm_eval --model togetherai-chat-completions \
+    #     --model_args "pretrained=$MODEL, max_tokens=1024" \
+    #     --tasks jp_new \
+    #     --num_fewshot 0 \
+    #     --batch_size auto \
+    #     --output_path ../results/jp \
+    #     --hf_hub_log_args "hub_results_org=TheFinAI,details_repo_name=lm-eval-results-jp,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False" \
+    #     --log_samples \
+    #     --apply_chat_template \
+    #     --include_path ../tasks/jp
         
     # # api-deepseek
     # lm_eval --model deepseek-chat-completions \
